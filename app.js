39d3437e9cfdd1bcd938e6032a25477f35aadd09
Omkar Arora <aroraomkar@gmail.com>
2020-12-04 13:47:15 +0530

    style updates, loader added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
+var loader = document.querySelector(".loader");
+var translatedSection = document.querySelector(".translated-section");
 
+function init() {
+  loader.style.display = "none";
+}
+
+// MOCK API
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// ACTUAL API
 var serverURL = "https://api.funtranslations.com/translate/groot.json";
 
 function getTranslationURL(text) {
@@ -12,10 +21,23 @@ function getTranslationURL(text) {
 function clickEventHandler() {
   let inputText = textInput.value;
 
+  // Loader set to ON
+  loader.style.display = "block";
+
+  // Hide output section
+  translatedSection.style.display = "none";
+
   //call server for processing
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
+      // Turn loader off
+      loader.style.display = "none";
+
+      // Translated section now visible
+      translatedSection.style.display = "unset";
+
+      // Set translated text
       outputDiv.innerText = json.contents.translated;
     })
     .catch((error) => console.log("error occurred", error));
